a17b76a015cf132f0b682f539bfdc635aebc812d
Hrayr Derbedrossian <94538722+hrayrdb@users.noreply.github.com>
2024-01-09 02:46:07 +0300

    pickup cube

--- maze_solver/controllers/hrayr/hrayr.py
+++ maze_solver/controllers/hrayr/hrayr.py
@@ -244,14 +244,163 @@ def backward(time):
         wheel.setVelocity(-7.0)  # Adjust max velocity as needed
     robot.step(time * timestep)
 
+  
+  
+  
+  
+  
+  
     
 ######################################################################################    
+# THE BOX CODE
 
+#! Initialize arm motors.
+armMotors = []
+armMotors.append(robot.getDevice("arm1"))
+armMotors.append(robot.getDevice("arm2"))
+armMotors.append(robot.getDevice("arm3"))
+armMotors.append(robot.getDevice("arm4"))
+armMotors.append(robot.getDevice("arm5"))
+# Set the maximum motor velocity.
+armMotors[0].setVelocity(1.5) # maxVelocity = 1.5
+armMotors[1].setVelocity(1.5)
+armMotors[2].setVelocity(1.5)
+armMotors[3].setVelocity(0.5)
+armMotors[4].setVelocity(1.5)
+
+#! Initialize arm position sensors.
+# These sensors can be used to get the current 
+# joint position and monitor the joint movements.
+armPositionSensors = []
+armPositionSensors.append(robot.getDevice("arm1sensor"))
+armPositionSensors.append(robot.getDevice("arm2sensor"))
+armPositionSensors.append(robot.getDevice("arm3sensor"))
+armPositionSensors.append(robot.getDevice("arm4sensor"))
+armPositionSensors.append(robot.getDevice("arm5sensor"))
+for sensor in armPositionSensors:
+    sensor.enable(timestep)
+
+#! Initialize gripper motors.
+finger1 = robot.getDevice("finger::left")
+finger2 = robot.getDevice("finger::right")
+# Set the maximum motor velocity.
+finger1.setVelocity(1.5)
+finger2.setVelocity(1.5) # 0.03
+# Read the miminum and maximum position of the gripper motors.
+fingerMinPosition = finger1.getMinPosition()
+fingerMaxPosition = finger1.getMaxPosition()
+
+#! Generic forward motion function
+def forward(time):
+    for wheel in wheels:
+        wheel.setVelocity(7.0) # maxVelocity = 14.81
+    robot.step(time * timestep)
+
+def backward(time):
+    for wheel in wheels:
+        wheel.setVelocity(-7.0) # maxVelocity = 14.81
+    robot.step(time * timestep)
+
+#! Generic stop function
+def halt():
+    for wheel in wheels:
+        wheel.setVelocity(0.0)
+
+def fold_arms():
+    armMotors[0].setPosition(-2.9)
+    armMotors[1].setPosition(1.5)
+    armMotors[2].setPosition(-2.6)
+    armMotors[3].setPosition(1.7)
+    armMotors[4].setPosition(0)
+
+def stretch_arms():
+    armMotors[0].setPosition(2.9)
+    armMotors[1].setPosition(-1.0)
+    armMotors[2].setPosition(2.5)
+    armMotors[3].setPosition(-1.7)
+    armMotors[4].setPosition(0)
+
+def turn_around(time):
+#TODO: robot.step(70 * timestep) - produces a 90 degrees turn
+    wheels[0].setVelocity(14)
+    wheels[1].setVelocity(-14)
+    wheels[2].setVelocity(14)
+    wheels[3].setVelocity(-14)
+    robot.step(time * timestep)
+    # forward()
+
+def pick_up():
+    armMotors[1].setPosition(-1.1)
+    armMotors[2].setPosition(-0.9)
+    armMotors[3].setPosition(-1.1)
+    finger1.setPosition(fingerMaxPosition)
+    finger2.setPosition(fingerMaxPosition)
+
+    # Monitor the arm joint position to 
+    # detect when the motion is completed.
+    while robot.step(timestep) != -1:
+        if abs(armPositionSensors[3].getValue() - (-1.2)) < 0.1:
+        # Motion completed.
+            break
+    print("CLOSING GRIPPER")
+    finger1.setPosition(0.014)     # Close gripper.
+    finger2.setPosition(0.014)
+    print("GRIP CLOSED")
+    robot.step(50 * timestep)    # Wait until the gripper is closed.
+    armMotors[1].setPosition(0)    # Lift arm.
+    # Wait until the arm is lifted.
+    # robot.step(200 * timestep)
+    
+    
+def open_grippers():
+    finger1.setPosition(fingerMaxPosition)
+    finger2.setPosition(fingerMaxPosition)
+    robot.step(70 * timestep)
+
+def drop():
+    armMotors[1].setPosition(0.5)
+    armMotors[3].setPosition(1.3)
+    armMotors[2].setPosition(1)
+    while robot.step(timestep) != -1:
+
+        if abs(armPositionSensors[3].getValue() - (-1.2)) > 2.5:
+        # Motion completed.
+            print("READY TO OPEN GRIPPERS")
+            break
+
+def hand_up():
+    armMotors[0].setPosition(0)
+    armMotors[1].setPosition(0)
+    armMotors[2].setPosition(0)
+    armMotors[3].setPosition(0)
+    armMotors[4].setPosition(0)
+    finger1.setPosition(fingerMaxPosition)
+    finger2.setPosition(fingerMaxPosition)
+
+
+
+
+
+
+
+
+
+
+#######################################################################################
 # Main robot control loop
 while robot.step(timestep) != -1:
     if maze_solved:
         print("Exiting the program.")
         break  # Exit the loop if the maze is solved
     
-    follow_line_pid()  # Follow the line
-   
+    # follow_line_pid()  
+    # forward(520)
+    halt()
+    pick_up()
+    forward(300)
+    drop()
+    open_grippers()
+    hand_up()
+    break
+
+ 
